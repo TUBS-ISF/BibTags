ba852c6a7bd8ff647b29d3bf3c21443db366f9ad
Elias Kuiter <info@elias-kuiter.de>
2025-03-07 09:13:46 +0100

    Revert "improved search for python"
    Apparently, this was not the issue.
    
    This reverts commit 9a9e01db4f6d5e4a170d2273073d4736b83ff20b.

--- scripts/sort.sh
+++ scripts/sort.sh
@@ -1,23 +1,12 @@
 #!/usr/bin/env bash
-
-if command -v python3 &> /dev/null; then
-	PYTHON=python3
-else
-	PYTHON=python
-fi
 python_env="python_bibtags"
+
 if [ -d "python/${python_env}" ]; then
-	source python/${python_env}/bin/activate
+    source python/${python_env}/bin/activate
 else
-	if command -v pip3 &> /dev/null; then
-		PIP=pip3
-	else
-		PIP=pip
-	fi
-	$PYTHON -m pip install --upgrade pip
-	$PYTHON -m venv python/${python_env}
-	source python/${python_env}/bin/activate
-	$PIP install -r python/requirements.txt
+    python3 -m venv python/${python_env}
+    source python/${python_env}/bin/activate
+    pip3 install -r python/requirements.txt
 fi
 
 python3 python/sort.py
