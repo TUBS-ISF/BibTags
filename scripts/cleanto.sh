e4ee07c6b61b1e7d635656cd7937c69850cdbcd9
Elias Kuiter <info@elias-kuiter.de>
2025-03-07 08:34:00 +0100

    added sanity checks for the parameter

--- scripts/cleanto.sh
+++ scripts/cleanto.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
+set -e
+
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 <output-dir> [options]"
+    exit 1
+fi
+
+if [ ! -d "$1" ]; then
+    echo "Error: $1 is not a directory"
+    exit 1
+fi
+
 java -cp mibtex/mibtex-cleaner.jar de.mibtex.BibtexCleaner "../literature/literature.bib" -o "$1/literature-cleaned.bib" "${@:2}"
 cp "../literature/MYabrv.bib" "$1"
 cp "../literature/MYshort.bib" "$1"
