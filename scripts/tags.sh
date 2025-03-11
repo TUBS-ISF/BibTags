2d28f81236abac87988f955206657305aebfe780
Elias Kuiter <info@elias-kuiter.de>
2025-03-11 08:30:08 +0100

    adapted tags script to new repository

--- scripts/tags.sh
+++ scripts/tags.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-# Usage: ./find_tags.sh [tt|ek|...]
+# Usage: ./tags.sh [tt|ek|...]
 
-# This script finds all tags used by one workgroup member in the literature.bib file.
+# This script finds all tags used by the given workgroup member in the literature.bib file.
 # The workgroup member is specified by the first argument, which consists of the first letters of the first and last name.
 # The script is useful to quickly look up which tags are available, so as to avoid duplication.
 
 who=${1:-"ek"}
-grep "$who-tags" ../literature/literature.bib | cut -d= -f2- | tr -d '{}' | tr , '\n' | grep -E .+ | sort | uniq
+grep "${who}tags" "$(dirname "$0")"/../literature/literature.bib | cut -d= -f2- | tr -d '{}' | tr , '\n' | grep -E .+ | sort | uniq
